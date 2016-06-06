6263bdfc605ef415b6abc6469ecd1db45dcba176
pcummuskey <pcummuskey@bravurasolutions.com>
2016-06-07 09:23:10 +1200

    Added some more implementation hints.

---
+++ src/main/server/input/google.js
@@ -0,0 +1,12 @@
+/* global require, module */
+'use strict';
+
+const vodoun = require('vodoun');
+
+vodoun.register('input_google', [], (service, deps) => {
+
+	// Fetch
+	// GET https://www.google.com/m8/feeds/contacts/{userEmail}/full?orderby=lastmodified&showdeleted=true&sortorder=descending
+
+});
+
---
+++ src/main/server/output/google.js
@@ -0,0 +1,15 @@
+/* global require, module */
+'use strict';
+
+const vodoun = require('vodoun');
+
+vodoun.register('output_google', [], (service, deps) => {
+
+	// Create
+	// POST https://www.google.com/m8/feeds/contacts/{userEmail}/full
+
+	// Update
+	// PUT https://www.google.com/m8/feeds/contacts/{userEmail}/full/{contactId}
+
+});
+
